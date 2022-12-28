5ea0d9fc1c8fcf59147ecbf0c5ce75c64b7856d7
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-12-28 15:48:16 +0800

    chore(deps): remove commitlint deps
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,16 +4,6 @@ const Configuration = {
      * Referenced packages must be installed
      */
     extends: ['@commitlint/config-conventional'],
-    /*
-     * Resolve and load conventional-changelog-atom from node_modules.
-     * Referenced packages must be installed
-     
-    parserPreset: 'conventional-changelog-atom',
-    /*
-     * Resolve and load @commitlint/format from node_modules.
-     * Referenced package must be installed
-     
-    formatter: '@commitlint/format',*/
 };
 
 module.exports = Configuration;
