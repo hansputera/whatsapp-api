312f612d5a08acf409c6559acb358403196255f3
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-12-28 15:45:30 +0800

    feat: initial
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import {defineConfig} from 'tsup';
+
+export default defineConfig({
+    bundle: true,
+    entry: ['./src/index.ts'],
+    clean: true,
+    minify: true,
+    minifySyntax: true,
+    outDir: './dist',
+    platform: 'node',
+    target: ['node14', 'node16', 'node18'],
+    tsconfig: './tsconfig.json',
+});
