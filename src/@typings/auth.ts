aef054804314124204dde5f0249952c3d8d2010c
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-12-29 21:54:55 +0800

    feat: add jwt service, and auth types
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

---
+++ src/@typings/auth.ts
@@ -0,0 +1,4 @@
+export type AuthSigned = {
+    key: string; // Base64 encoded
+    iv: string; // Base64 encoded
+};
---
+++ src/services/jwt.ts
@@ -0,0 +1,32 @@
+import * as jwt from 'jsonwebtoken';
+import type {AuthSigned} from '@typings/auth';
+
+export const createAuthToken = (auth: AuthSigned): string => {
+    const token = jwt.sign(auth, process.env.JWT_SECRET!, {
+        issuer: '@hansputera/whatsapp-api-baileys',
+        expiresIn: '2d',
+        algorithm: 'HS512',
+    });
+
+    return token;
+};
+
+export const decodeAuthToken = async (token: string): Promise<AuthSigned> =>
+    new Promise((resolve, reject) => {
+        jwt.verify(
+            token,
+            process.env.JWT_SECRET!,
+            {
+                issuer: '@hansputera/whatsapp-api-baileys',
+                algorithms: ['HS512'],
+            },
+            (err, decoded) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                resolve(decoded as AuthSigned);
+            },
+        );
+    });
